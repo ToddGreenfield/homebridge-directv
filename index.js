e5501f3145b45103fb42b5be9919a1e75f6abc7e
toddgreenfield <todd.greenfield@me.com>
2017-02-19 15:53:10 -0500

    Changes for 0.0.6, added channel and exclude_geni.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 //         "platform": "Directv",				// required
 //         "name": "DTV",						// Optional - defaults to DTV
 //         "ip_address": "IP of Primary STB", 	// required
+//		   "exclude_geni": true					// Optional - defaults to false
 //     }
 // ], 
 //
@@ -15,15 +16,17 @@
 //
 //
 
+var inherits = require('util').inherits;
 var DirecTV = require('directv-remote');
 var titleCase = require('title-case');
 var remote;
 
 function DirectvPlatform(log, config){
 	this.config = config;
-    this.ip_address = config["ip_address"];
+	this.ip_address = config["ip_address"];
 	this.name = config["name"] || 'DTV';
-    this.log = log;
+	this.excludeGeni = config["exclude_geni"] || false;
+	this.log = log;
 
 	if (!this.ip_address) throw new Error("DTV - You must provide a config value for 'ip_address'.");
 
@@ -41,9 +44,13 @@ DirectvPlatform.prototype = {
 			if (!err) {
 				that.log( "Finding DTV Locations (accessories)...");
 				for(var i=0; i <response.locations.length; ++i){
-					that.log( "Found DTV Location %s", response.locations[i].locationName);
-					var accessory = new DirectvSTBAccessory(that.log, response.locations[i], that.config);
-					foundAccessories.push(accessory);
+					if ((!that.excludeGeni) || (response.locations[i].clientAddr == '0')) {
+						that.log( "Found DTV Location %s", response.locations[i].locationName);
+						var accessory = new DirectvSTBAccessory(that.log, response.locations[i], that.config);
+						foundAccessories.push(accessory);
+					} else {
+						that.log( "Found DTV Location %s, but EXCLUDING per configuration setting.", response.locations[i].locationName);
+					}
 				}
 				callback(foundAccessories);
 			} else {
@@ -70,29 +77,61 @@ DirectvSTBAccessory.prototype = {
     getRemote: function(type, callback){
         var that = this;
 		var status;
-		remote.getMode(that.clientAddr, function (err, response) {
-			if (err || response.mode == "1") {
-				that.log('DTV location %s power state is currently OFF or UNREACHABLE', that.location);
-				callback(null, false);
-				return;
-			} else if (response.mode == "0") {
-				that.log('DTV location %s power state is currently ON', that.location);
-				callback(null, true);
-			}
-		});
+		switch(type) {
+			case "power":
+				remote.getMode(that.clientAddr, function (err, response) {
+					if (err || response.mode == "1") {
+						that.log('DTV location %s power state is currently OFF or UNREACHABLE', that.location);
+						callback(null, false);
+						return;
+					} else if (response.mode == "0") {
+						that.log('DTV location %s power state is currently ON', that.location);
+						callback(null, true);
+					}
+				});
+			break;
+			case "channel":
+				remote.getTuned(that.clientAddr, function (err, response) {
+					if (err) {
+						that.log('Unable to call for current channel.');
+						callback(new Error("STB Get Tuned Error."), false);
+						return;
+					} else {
+						that.log('Current Channel is %d', parseInt(response.major))
+						callback(null, parseInt(response.major));
+					}
+				});
+			break;
+		}	
 	},
-    setRemotePower: function(state, callback){
+    setRemote: function(type, value, callback){
         var that = this;
-		remote.processKey('power', that.clientAddr, function(err, reponse) {
-			if (err) {
-				that.log('Unable to change DTV location %s power state!', that.location);
-				callback(new Error("STB not responding."), false);
-				return;
-			} else {
-				that.log('DTV location %s power state is now: %s', that.location, (state) ? 'ON' : 'OFF');
-                callback();
-            }
-        });
+		switch(type) {
+			case "power":
+				remote.processKey('power', that.clientAddr, function(err, response) {
+					if (err) {
+						that.log('Unable to change DTV location %s power state!', that.location);
+						callback(new Error("STB Process Power Error."), false);
+						return;
+					} else {
+						that.log('DTV location %s power state is now: %s', that.location, (value) ? 'ON' : 'OFF');
+						callback();
+					}
+				});
+			break;
+			case "channel":
+				remote.tune(value, that.clientAddr, function(err, response) {
+					if (err) {
+						that.log('Unable to change DTV location %s Channel because %s', that.location, err);
+						callback(new Error("STB Process Channel Error."), false);
+						return;
+					} else {
+						that.log('DTV location %s Channel is now: %s', that.location, value);
+						callback();
+					}
+				});
+			break;
+		}	
     },
     getServices: function() {
         var that = this;
@@ -101,8 +140,12 @@ DirectvSTBAccessory.prototype = {
 
 		this.service.getCharacteristic(Characteristic.On)
 			.on('get', function(callback) { that.getRemote("power", callback);})
-			.on('set', function(value, callback) {that.setRemotePower(value, callback);});
+			.on('set', function(value, callback) {that.setRemote("power", value, callback);});
 
+		this.service.addCharacteristic(ChannelCharacteristic)
+			.on('get', function(callback) { that.getRemote("channel", callback);})
+			.on('set', function(value, callback) {that.setRemote("channel", value, callback);});		
+			
         services.push(this.service);
 
         var service = new Service.AccessoryInformation();
@@ -121,12 +164,33 @@ DirectvSTBAccessory.prototype = {
 module.exports.accessory = DirectvSTBAccessory;
 module.exports.platform = DirectvPlatform;
 
-var Service, Characteristic;
+var Service, Characteristic, ChannelCharacteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
+// we can only do this after we receive the homebridge API object
+  makeChannelCharacteristic();
+  
   homebridge.registerAccessory("homebridge-directv-location", "DirectvSTB", DirectvSTBAccessory);
   homebridge.registerPlatform("homebridge-directv", "Directv", DirectvPlatform);
 };
+
+function makeChannelCharacteristic() {
+
+  ChannelCharacteristic = function () {
+    Characteristic.call(this, 'Channel', '212131F4-2E14-4FF4-AE13-C97C3232499D');
+    this.setProps({
+      format: Characteristic.Formats.INT,
+      unit: Characteristic.Units.NONE,
+      maxValue: 500,
+      minValue: 0,
+      minStep: 1,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+
+  inherits(ChannelCharacteristic, Characteristic);
+}
